31265cc9b9d7281f7f64a3c7ca85b33bc976f9d6
Allan Goto <allanyujigoto726@gmail.com>
2022-09-25 17:42:47 -0300

    Fix React

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import App from './App'
 import './reset.css'
 import './index.css'
 
+window.React = React
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
